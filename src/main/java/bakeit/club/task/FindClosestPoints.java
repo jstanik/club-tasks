df4026b9067e9625f24387684ca126b405071248
Juraj Staník <juraj.stanik@gmail.com>
2023-11-08 14:43:40 +0100

    Add task to find the closest points.

---
+++ src/main/java/bakeit/club/task/FindClosestPoints.java
@@ -0,0 +1,41 @@
+package bakeit.club.task;
+
+public class FindClosestPoints {
+
+  /**
+   * Implements a method which goes through the array {@code points} and find two points which
+   * are closest to each other.
+   *
+   * @param points the array of at least two points
+   * @return the pair of points that are closest to each other
+   */
+  PointPair findClosestPoints(Point[] points) {
+    if (points.length < 2) {
+      throw new IllegalArgumentException("The length of the array 'points' has to be at least 2");
+    }
+
+    // TODO replace the following line with the logic
+    throw new UnsupportedOperationException();
+  }
+
+}
+
+class Point {
+  double x;
+  double y;
+
+  Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class PointPair {
+  Point p1;
+  Point p2;
+
+  PointPair(Point p1, Point p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+  }
+}
---
+++ src/test/java/bakeit/club/task/FindClosestPointsTest.java
@@ -0,0 +1,60 @@
+package bakeit.club.task;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FindClosestPointsTest {
+
+
+  @Test
+  void findClosestPoints() {
+    int pointCount = Utils.randomInt(4, 20);
+
+    Point[] points = new Point[pointCount];
+    for (int i = 0; i < points.length; i++) {
+      points[i] = new Point(Utils.randomDouble(0, 10), Utils.randomDouble(0, 10));
+    }
+
+    FindClosestPoints cut = new FindClosestPoints();
+
+    PointPair actual = cut.findClosestPoints(points);
+
+    List<PointPair> expected = possibleResults(points);
+
+    assertTrue(expected.stream().anyMatch(pair ->
+        (pair.p1 == actual.p1 && pair.p2 == actual.p2)
+            || (pair.p2 == actual.p1 && pair.p1 == actual.p2)));
+  }
+
+  private List<PointPair> possibleResults(Point[] points) {
+    List<PointPair> pairs = new ArrayList<>();
+
+    double shortestDistance = Double.MAX_VALUE;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      for (int j = i + 1; j < points.length ; j++) {
+        PointPair pair = new PointPair(points[i], points[j]);
+        double distance = distance(pair);
+        if (distance < shortestDistance) {
+          pairs.clear();
+          pairs.add(pair);
+        } else if( distance == shortestDistance) {
+          pairs.add(pair);
+        }
+      }
+    }
+
+    return pairs;
+  }
+
+  private double distance(PointPair pair) {
+    double deltaX = pair.p1.x - pair.p2.x;
+    double deltaY = pair.p1.y - pair.p2.y;
+
+    return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+  }
+}
